315c230d64c4b3eeacc197eea8e3cb73c0c75af9
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2024-09-02 02:22:52 +0300

    feat: first version of app logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,59 @@
 import "./styles.css";
-console.log("test");
+
+const projects = [];
+let currentProject;
+let currentTask;
+class Project {
+    items = [];
+    constructor(name, tags) {
+        this.name = name;
+        this.tags = tags;
+    }
+    MakeCurrentProject() {
+        currentProject = this;
+    }
+}
+class Task {
+    list = [];
+    constructor(name, tags) {
+        this.name = name;
+        this.tags = tags;
+    }
+    addTextBlock(text, type, margin) {
+        this.list.push(new TextBlock(text, type, margin));
+    }
+    MakeCurrentTask() {
+        currentTask = this;
+    }
+}
+
+class TextBlock {
+    constructor(text, type, margin) {
+        this.text = text;
+        this.type = type;
+        this.margin = margin;
+    }
+    switchType() {
+        if (this.type === "checkbox") {
+            this.type = "plainText";
+        }
+        else {
+            this.type = "checkbox";
+        }
+    }
+    changeText(newText) {
+        this.text = newText;
+    }
+    changeMargin(newMargin) {
+        this.margin = newMargin;
+    }
+}
+
+function createProject(name) {
+    projects.push(new Project(name));
+    currentProject = projects[projects.length-1];
+}
+
+function createTask() {
+    currentProject.items.push(new Task("testing"));
+}
