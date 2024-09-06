106b04c2860bf1dd3e542b5e071b9c1e2aaa8d3d
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2024-09-06 19:47:39 +0300

    feat: add json parser method(getLocalStorage)
    this method allow to get all data from localStorage and recreate all
    instances of any classes.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,37 @@ import { ui } from "./ui.js";
 import { listeners } from "./listeners.js";
 import { Project } from "./core.js";
 
+function getLocalStorage() {
+    let data = JSON.parse(localStorage.getItem("projects"));
+    for (let i=0; i<data.length; i++) {
+        Project.createProject(
+            data[i].name, 
+            data[i].tags);
+        for(let j=0; j<data[i].items.length; j++) {
+            Project.getProjectsList()[i].createTask(
+                data[i].items[j].name, 
+                data[i].items[j].tags);
+            for (let k=0; k<data[i].items[j].list.length; k++) {
+                Project.getProjectsList()[i].items[j].addTextBlock(
+                    data[i].items[j].list[k].text, 
+                    data[i].items[j].list[k].type, 
+                    data[i].items[j].list[k].margin);
+            }
+        }
+    }
+}
+
 // ui();
 // listeners();
 
-Project.createProject("test");
-Project.getProjectsList()[0].setCurrentProject();
-Project.getCurrentProject().createTask("tost");
-Project.getCurrentProject().getTaskList()[0].addTextBlock("blabla", "checkbox", "0");
-Project.getCurrentProject().getTaskList()[0].getTextList();
-Project.getCurrentProject().getTaskList()[0].getTextList()[0].changeText("ogogo");
-Project.getCurrentProject().getTaskList()[0].getTextList();
-Project.getProjectsList();
+// Project.createProject("test");
+// Project.getProjectsList()[0].setCurrentProject();
+// Project.getCurrentProject().createTask("tost");
+// Project.getCurrentProject().getTaskList()[0].addTextBlock("blabla", "checkbox", "0");
+// Project.getCurrentProject().getTaskList()[0].getTextList();
+// Project.getCurrentProject().getTaskList()[0].getTextList()[0].changeText("ogogo");
+// Project.getCurrentProject().getTaskList()[0].getTextList();
+// Project.createProject("test2");
+// Project.setLocalStorage("projects", JSON.stringify(Project.getProjectsList()));
+// getLocalStorage();
+// console.log(Project.getProjectsList());
